0407b78b0fbe2e7d0bde9303601efe1daf1bcbd4
Steve Lopez <coderstudent290@gmail.com>
2013-06-17 18:02:21 -0600

    changed form view and added a method to client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,9 @@
 class Client < ActiveRecord::Base
   attr_accessible :city, :client1_age, :client1_email, :client1_gender, :client1_name, :client1_phone, :client2_age, :client2_email, :client2_gender, :client2_name, :client2_phone, :country, :home_study_completed, :number_dependent_children, :postal_code, :state, :street1, :street2, :will_travel
   has_many :inquiries
+
+	def both_names
+		"#{client1_name}, #{client2_name}"
+	end
 end
+
