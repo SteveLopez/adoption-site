d61efc66fc6b77c9424f8e7010f8078f740ab1ee
Steve Lopez <coderstudent290@gmail.com>
2013-06-26 16:31:30 -0600

    Updated a bunch of stuff for Adoption site.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    render :index, :layout => "main"
   end
 end
--- app/models/child.rb
+++ app/models/child.rb
@@ -1,4 +1,15 @@
 class Child < ActiveRecord::Base
   attr_accessible :country_of_origin, :date_of_birth, :eligible, :gender, :name
   has_many :inquiries
+  
+  def age
+    if Time.now.yday > date_of_birth.yday
+      Time.now.year - date_of_birth.year
+    else 
+      Time.now.year - date_of_birth.year - 1
+    end
+  end
+
+
 end
+
