ff5e0ba6cae612779c0eef98e85370507a6018a7
Steve Lopez <coderstudent290@gmail.com>
2013-06-27 12:51:56 -0600

    More additions and updates to Adoption site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper_method :current_user
+
+  		private
+
+  		def current_user
+    			@current_user ||= User.find_by_id(session[:user_id])
+  		end
+
+  		def authenticate_user!
+    			unless current_user
+        			redirect_to "/sessions/new"
+    			end
+  		end
+
 end
+
+
