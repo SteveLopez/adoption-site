5e30b00daca734076a0fd3ca9df32580ce9fa8cc
Steve Lopez <coderstudent290@gmail.com>
2013-07-01 11:00:16 -0600

    Carrierwave image loader

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,6 +1,7 @@
 class Child < ActiveRecord::Base
-  attr_accessible :country_of_origin, :date_of_birth, :eligible, :gender, :name
+  attr_accessible :country_of_origin, :date_of_birth, :eligible, :gender, :name, :image
   has_many :inquiries
+  mount_uploader :image, ImageUploader
   
   def age
     if Time.now.yday > date_of_birth.yday
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130627194343) do
+ActiveRecord::Schema.define(:version => 20130630050042) do
 
   create_table "agencies", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130627194343) do
     t.boolean  "eligible"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+    t.string   "image"
   end
 
   create_table "clients", :force => true do |t|
